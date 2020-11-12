a1c7568bf1ab2ce6a09a4268accecfc2496a21cb
Chih-Yuan Su <csu68@gatech.edu>
2020-11-12 11:50:36 -0800

    Finished the implementation of the rank cal

--- src/server/server.js
+++ src/server/server.js
@@ -23,14 +23,18 @@ app.use(cors());
 var mongoUrl = "mongodb://localhost:27017/armyListTest";
 // connect to mongodb instance where database is testdb
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
 
 //get users
 app.get("/api/armyUserList/", (req, res) => {
   // use find() method to return all Users
-  User.find((err, result) => {
+  User.find({}, (err, result) => {
     if (err) {
-      console.log(err);
+      console.log("Failed to find all: " + err);
       res.status(500).send(err);
     } else {
       res.json(result);
@@ -43,14 +47,14 @@ app.post("/api/armyUserList/", (req, res) => {
   let newUser = new User(req.body);
   let error = newUser.validateSync();
   if (error) {
-    console.log(error);
+    console.log("Failed to validate: " + error);
     res.status(500).send(error);
     return;
   }
   // save new user to db
   newUser.save((err, result) => {
     if (err) {
-      console.log(err);
+      console.log("Failed to save: " + err);
       res.status(500).send(err);
     } else {
       res.sendStatus(200);
@@ -58,72 +62,103 @@ app.post("/api/armyUserList/", (req, res) => {
   });
 });
 
-const computeUserRankNum = (all_users) => {
+function computeAllDSNum(all_users, res) {
   // first establish the superior pointer such that
   // we build a initial linked list
   for (let i = 0; i < all_users.length; i++) {
     let this_user = all_users[i];
     if (this_user.superiorID) {
       this_user.superior = all_users.find(
-        (user) => user._id === this_user.superiorID
+        (user) => user.id === this_user.superiorID
       );
-      console.log(this_user.superior);
+      this_user.superior.hasDep = true;
     } else {
       this_user.superior = null;
     }
+    this_user.idInt = i;
   }
 
   // traverse again to find who is has the maximum
   let max_rank = 0;
-  let max_rank_user = -1;
+  // let max_rank_user = -1;
   for (let i = 0; i < all_users.length; i++) {
     let this_user = all_users[i];
     let this_rank = 0;
+    let visited = new Set();
+    visited.add(this_user.idInt);
+
     while (this_user.superior) {
-      console.log(this_user.superiorID);
       this_user = this_user.superior;
+      if (visited.has(this_user.idInt)) {
+        break;
+      }
+      visited.add(this_user.idInt);
       this_rank++;
     }
+
     if (this_rank > max_rank) {
       max_rank = this_rank;
-      max_rank_user = i;
+      // max_rank_user = i;
     }
     all_users[i].DSNum = this_rank;
   }
 
-  console.log(max_rank);
-
-  let userToUpdate = [];
-
-  for (let i = 0; i < all_users.length; i++) {
-    if (all_users[i].DSNum > 0) {
-      all_users[i].DSNum = max_rank - all_users[i].DSNum;
-      userToUpdate.push(all_users[i]);
-    }
-  }
-
-  if (max_rank_user >= 0) {
-    // set the max rank user
-    all_users[max_rank_user].DSNum = max_rank;
-    userToUpdate.push(all_users[max_rank_user]);
-  }
-
   // reverse the rank
-  User.updateMany(userToUpdate);
-};
-
-const updateAllUserDsNum = (res) => {
-  User.find((err, result) => {
-    if (err) {
-      console.log(err);
+  const tasks = [];
+  for (let i = 0; i < all_users.length; i++) {
+    if (all_users[i].hasDep) {
+      let this_user = all_users[i];
+      this_user.DSNum = max_rank - this_user.DSNum;
+      tasks.push(() => {
+        User.findByIdAndUpdate(
+          this_user._id,
+          {
+            DSNum: this_user.DSNum,
+          },
+          (err, result) => {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+      });
     }
-    //
-    console.log("updating All");
-    computeUserRankNum(result);
-    // console.log(typeof result);
-    // result.updateAll();
+  }
 
+  // if (max_rank_user >= 0) {
+  //   let max_ran_user = all_users[max_rank_user];
+  //   // set the max rank user
+  //   max_ran_user.DSNum = max_rank;
+  //   tasks.push(() => {
+  //     User.findByIdAndUpdate(
+  //       max_ran_user._id,
+  //       {
+  //         DSNum: max_ran_user.DSNum,
+  //       },
+  //       (err, result) => {
+  //         if (err) {
+  //           console.log(err);
+  //         }
+  //       }
+  //     );
+  //   });
+  // }
+
+  const arrayOfPromises = tasks.map((task) => task());
+
+  // call Promise.all on that array
+  Promise.all(arrayOfPromises).then((result) => {
     res.sendStatus(200);
+  });
+}
+
+const findAllAndComputeDSNum = (res) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      computeAllDSNum(users, res);
+    }
   });
 };
 
@@ -134,20 +169,20 @@ app.post("/api/armyUserList/:id", (req, res) => {
     return;
   }
 
-  User.findById(req.params.id, function (err, doc) {
-    if (err) {
-      console.log(err);
-      res.status(500).send(err);
+  User.findById(req.params.id, function (find_err, doc) {
+    if (find_err) {
+      console.log("Failed to find: " + find_err);
+      res.status(500).send(find_err);
     } else {
       let needToUpdate = req.body.superiorID !== doc.superiorID;
-      doc.updateOne(req.body, (err2) => {
-        if (err2) {
-          console.log(err2);
-          res.status(500).send(err2);
+      doc.updateOne(req.body, (update_error) => {
+        if (update_error) {
+          console.log("Failed to update: " + update_error);
+          res.status(500).send(update_error);
           return;
         }
         if (needToUpdate) {
-          updateAllUserDsNum(res);
+          findAllAndComputeDSNum(res);
         } else {
           res.sendStatus(200);
         }
@@ -158,7 +193,6 @@ app.post("/api/armyUserList/:id", (req, res) => {
 
 // delete user
 app.delete("/api/armyUserList/:id", (req, res) => {
-  console.log(req.params);
   if (req.params.id === null || req.params.id === "") {
     res.status(500).send("Invalid Id");
     return;
@@ -166,7 +200,7 @@ app.delete("/api/armyUserList/:id", (req, res) => {
 
   User.findByIdAndRemove(req.params.id, function (err, docs) {
     if (err) {
-      console.log(err);
+      console.log("Failed to remove" + err);
       res.status(500).send(err);
     } else {
       console.log("Removed: " + req.params.id);
